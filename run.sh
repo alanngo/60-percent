9b3f90a5ca670f3499c09d18da890a36a52d43ac
alan <alanngo673@gmail.com>
2024-12-25 17:52:36 -0600

    syntax fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 SRC_FILE=$1 
 OPTIONS=$2
-if [ -z "$SRC_FILE"]; then
+if [ -z "$SRC_FILE" ]; then
     echo "Choose a keyboard first!"
 else
     bash "$HOME/60percent/keyboards/$SRC_FILE.sh" $OPTIONS
