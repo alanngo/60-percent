80ecd9020b0d089f8e109ef9895539c6b5de946f
alan <alanngo673@gmail.com>
2024-12-25 17:57:06 -0600

    usage

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 SRC_FILE=$1 
 OPTIONS=$2
+BASE_DIR="$HOME/60percent"
 if [ -z "$SRC_FILE" ]; then
     echo "Choose a keyboard first!"
+    ls "$BASE_DIR/keyboards"
 else
-    bash "$HOME/60percent/keyboards/$SRC_FILE.sh" $OPTIONS
+    bash "$BASE_DIR/keyboards/$SRC_FILE.sh" $OPTIONS
 fi
